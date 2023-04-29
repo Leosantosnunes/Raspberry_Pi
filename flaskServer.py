b8b778418b6b4190cbf91f7ba965c6a061589fd2
Leosantosnunes <leonardon09@gmail.com>
2023-04-29 11:31:29 -0400

    Add content about raspberry monitor

--- flaskServer.py
+++ flaskServer.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, Response
 import datetime
 import os
 import RPi.GPIO as GPIO
+import psutil 
 
 
 GPIO.setmode(GPIO.BOARD)
@@ -13,15 +14,28 @@ GPIO.setup(11,GPIO.OUT)
 app=Flask(__name__)
     
 @app.route('/')
-def index():
-    #return 'hello world!'
+def index():    
     now=datetime.datetime.now()
     timeString=now.strftime("%Y-%m-%d %H:%M")
     data = GPIO.setup(11,GPIO.OUT)
+    memory = psutil.virtual_memory()
+    disk = psutil.disk_usage('/')
     templateData={
         'title':'Indoor Farm Management - RaspBerry Pi 3 A+',
         'time':timeString,
         'data':data,
+        'cpu_percent': psutil.cpu_percent(1),
+        'cpu_count': psutil.cpu_count(),
+        'cpu_freq': psutil.cpu_freq(),
+        'cpu_mem_total': memory.total,
+        'cpu_mem_avail': memory.available,
+        'cpu_mem_used': memory.used,
+        'cpu_mem_free': memory.free,
+        'disk_usage_total': disk.total,
+        'disk_usage_used': disk.used,
+        'disk_usage_free': disk.free,
+        'disk_usage_percent': disk.percent,
+        'sensor_temperatures': psutil.sensors_temperatures()['cpu-thermal'][0].current,        
     }    
     return render_template('index.html',**templateData)
 
